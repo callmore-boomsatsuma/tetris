5a405ddbd131342153aef58d49c2ef7521a00c44
callmore <22885888+Callmore@users.noreply.github.com>
2026-06-05 21:56:33 +0100

    weird bug fixing???

--- scenes/piece_display/piece_display.gd
+++ scenes/piece_display/piece_display.gd
@@ -3,6 +3,10 @@ extends Panel
 
 @export var displayed_piece: PieceInfo:
 	set(value):
+		if not is_node_ready():
+			return
 		$%PieceVisual.piece = value
 	get:
-		return $%PieceVisual.piece
+		if not is_node_ready():
+			return
+		return $%PieceVisual.piece 
--- scripts/piece_icon_visual.gd
+++ scripts/piece_icon_visual.gd
@@ -2,5 +2,7 @@
 extends "res://scripts/piece_visual.gd"
 
 func _draw() -> void:
+	if piece == null:
+		return
 	draw_set_transform(piece.display_offset * cell_size, 0, Vector2.ONE)
 	super._draw()
